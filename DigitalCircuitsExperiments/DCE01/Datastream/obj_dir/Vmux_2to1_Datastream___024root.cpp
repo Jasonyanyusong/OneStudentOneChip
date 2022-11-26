51baa2bc6031be7ab33f35fa1d71a56ec32df88d
22041384-Yan Yusong <jasonyanyusong@outlook.com>
2022-11-26 12:18:51 +0800

    Regenerated DCE01A Files using verilator v4.210.

---
+++ DigitalCircuitsExperiments/DCE01/Datastream/obj_dir/Vmux_2to1_Datastream___024root.cpp
@@ -0,0 +1,60 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vmux_2to1_Datastream.h for the primary calling header
+
+#include "Vmux_2to1_Datastream___024root.h"
+#include "Vmux_2to1_Datastream__Syms.h"
+
+//==========
+
+VL_INLINE_OPT void Vmux_2to1_Datastream___024root___combo__TOP__1(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___combo__TOP__1\n"); );
+    // Body
+    vlSelf->y = (((~ (IData)(vlSelf->s)) & (IData)(vlSelf->a)) 
+                 | ((IData)(vlSelf->s) & (IData)(vlSelf->b)));
+}
+
+void Vmux_2to1_Datastream___024root___eval(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___eval\n"); );
+    // Body
+    Vmux_2to1_Datastream___024root___combo__TOP__1(vlSelf);
+}
+
+QData Vmux_2to1_Datastream___024root___change_request_1(Vmux_2to1_Datastream___024root* vlSelf);
+
+VL_INLINE_OPT QData Vmux_2to1_Datastream___024root___change_request(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___change_request\n"); );
+    // Body
+    return (Vmux_2to1_Datastream___024root___change_request_1(vlSelf));
+}
+
+VL_INLINE_OPT QData Vmux_2to1_Datastream___024root___change_request_1(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___change_request_1\n"); );
+    // Body
+    // Change detection
+    QData __req = false;  // Logically a bool
+    return __req;
+}
+
+#ifdef VL_DEBUG
+void Vmux_2to1_Datastream___024root___eval_debug_assertions(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___eval_debug_assertions\n"); );
+    // Body
+    if (VL_UNLIKELY((vlSelf->a & 0xfeU))) {
+        Verilated::overWidthError("a");}
+    if (VL_UNLIKELY((vlSelf->b & 0xfeU))) {
+        Verilated::overWidthError("b");}
+    if (VL_UNLIKELY((vlSelf->s & 0xfeU))) {
+        Verilated::overWidthError("s");}
+}
+#endif  // VL_DEBUG
---
+++ DigitalCircuitsExperiments/DCE01/Datastream/obj_dir/Vmux_2to1_Datastream___024root__Slow.cpp
@@ -0,0 +1,59 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vmux_2to1_Datastream.h for the primary calling header
+
+#include "Vmux_2to1_Datastream___024root.h"
+#include "Vmux_2to1_Datastream__Syms.h"
+
+//==========
+
+
+void Vmux_2to1_Datastream___024root___ctor_var_reset(Vmux_2to1_Datastream___024root* vlSelf);
+
+Vmux_2to1_Datastream___024root::Vmux_2to1_Datastream___024root(const char* _vcname__)
+    : VerilatedModule(_vcname__)
+ {
+    // Reset structure values
+    Vmux_2to1_Datastream___024root___ctor_var_reset(this);
+}
+
+void Vmux_2to1_Datastream___024root::__Vconfigure(Vmux_2to1_Datastream__Syms* _vlSymsp, bool first) {
+    if (false && first) {}  // Prevent unused
+    this->vlSymsp = _vlSymsp;
+}
+
+Vmux_2to1_Datastream___024root::~Vmux_2to1_Datastream___024root() {
+}
+
+void Vmux_2to1_Datastream___024root___eval_initial(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___eval_initial\n"); );
+}
+
+void Vmux_2to1_Datastream___024root___combo__TOP__1(Vmux_2to1_Datastream___024root* vlSelf);
+
+void Vmux_2to1_Datastream___024root___eval_settle(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___eval_settle\n"); );
+    // Body
+    Vmux_2to1_Datastream___024root___combo__TOP__1(vlSelf);
+}
+
+void Vmux_2to1_Datastream___024root___final(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___final\n"); );
+}
+
+void Vmux_2to1_Datastream___024root___ctor_var_reset(Vmux_2to1_Datastream___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vmux_2to1_Datastream__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vmux_2to1_Datastream___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->a = VL_RAND_RESET_I(1);
+    vlSelf->b = VL_RAND_RESET_I(1);
+    vlSelf->s = VL_RAND_RESET_I(1);
+    vlSelf->y = VL_RAND_RESET_I(1);
+}
