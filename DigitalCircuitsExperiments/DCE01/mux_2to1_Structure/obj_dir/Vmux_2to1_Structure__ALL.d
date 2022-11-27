08206c6e6adad20a5cfd6dbbcfe54cf2acaee798
22041384-Yan Yusong <jasonyanyusong@outlook.com>
2022-11-27 14:43:04 +0800

    6th commit, generated simulated files using verilator with cc exe build trace debug

---
+++ DigitalCircuitsExperiments/DCE01/mux_2to1_Structure/obj_dir/Vmux_2to1_Structure__ALL.d
@@ -0,0 +1,12 @@
+Vmux_2to1_Structure__ALL.o: Vmux_2to1_Structure__ALL.cpp \
+ Vmux_2to1_Structure.cpp Vmux_2to1_Structure.h \
+ /usr/local/share/verilator/include/verilated_heavy.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ Vmux_2to1_Structure__Syms.h Vmux_2to1_Structure___024root.h \
+ /usr/local/share/verilator/include/verilated_vcd_c.h \
+ /usr/local/share/verilator/include/verilated_trace.h \
+ /usr/local/share/verilator/include/verilated_trace_defs.h \
+ Vmux_2to1_Structure___024root.cpp Vmux_2to1_Structure__Trace.cpp \
+ Vmux_2to1_Structure___024root__Slow.cpp Vmux_2to1_Structure__Syms.cpp \
+ Vmux_2to1_Structure__Trace__Slow.cpp
