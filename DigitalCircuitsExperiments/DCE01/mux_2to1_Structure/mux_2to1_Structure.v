664eb7506579b9ffd96aa563ffdf3477013f9191
22041384-Yan Yusong <jasonyanyusong@outlook.com>
2022-11-27 14:35:55 +0800

    Update the file with bug fixes

--- DigitalCircuitsExperiments/DCE01/mux_2to1_Structure/mux_2to1_Structure.v
+++ DigitalCircuitsExperiments/DCE01/mux_2to1_Structure/mux_2to1_Structure.v
@@ -3,7 +3,7 @@ module mux_2to1_Structure(a, b, s, y);
     output y;
     wire l, r, s_n;
     my_not i1(.a(s), .b(s_n));
-    my_and i2 (.a(s_n), .b(a), .c(a));
+    my_and i2 (.a(s_n), .b(a), .c(l));
     my_and i3 (.a(s), .b(b), .c(r));
     my_or i4 (.a(l), .b(r), .c(y));
 endmodule
