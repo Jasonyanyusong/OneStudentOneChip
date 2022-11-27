fb09ba2b0eb873482f083beb172b91f45c13d354
22041384-Yan Yusong <jasonyanyusong@outlook.com>
2022-11-27 14:26:39 +0800

    modified module names

--- DigitalCircuitsExperiments/DCE01/mux_2to1_Structure/mux_2to1_Structure.v
+++ DigitalCircuitsExperiments/DCE01/mux_2to1_Structure/mux_2to1_Structure.v
@@ -1,16 +1,16 @@
-module my_and (a, b, c);
+module one_and (a, b, c);
     input a, b;
     output c;
     assign c = a & b;
 endmodule
 
-module my_or (a, b, c);
+module two_or (a, b, c);
     input a, b;
     output c;
     assign c = a | b;
 endmodule
 
-module my_not (a, b);
+module three_not (a, b);
     input a;
     output b;
     assign b = ~a;
@@ -20,8 +20,8 @@ module mux_2to1_Structure(a, b, s, y);
     input a, b, s;
     output y;
     wire l, r, s_n;
-    my_not i1(.a(s), .b(s_n));
-    my_and i2 (.a(s_n), .b(a), .c(a));
-    ny_and i3 (.a(s), .b(b), .c(r));
-    my_or i4 (.a(l), .b(r), .c(y));
+    three_not i1(.a(s), .b(s_n));
+    one_and i2 (.a(s_n), .b(a), .c(a));
+    one_and i3 (.a(s), .b(b), .c(r));
+    two_or i4 (.a(l), .b(r), .c(y));
 endmodule
