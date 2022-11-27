8c811e965ee255c9a9ff913ea3cf8bad317b0073
22041384-Yan Yusong <jasonyanyusong@outlook.com>
2022-11-27 11:09:59 +0800

    Using structure modeling to create a 2 to 1 multiplexer

---
+++ DigitalCircuitsExperiments/DCE01/mux_2to1_Structure/mux_2to1_Structure.v
@@ -0,0 +1,12 @@
+module my_and (a, b, c);
+    input a, b;
+    output c;
+    assign c = a & b;
+endmodule
+
+module my_or (a, b, c);
+    input a, b;
+    output c;
+    assign c = a | b;
+endmodule
+
