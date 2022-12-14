de175657d432b18cd61e034e56dd003be08335e8
Jasonyanyusong <Jasonyanyusong@outlook.com>
2022-12-14 20:44:38 +0800

    Using verilator --cc --exe --build --trace --debug, simulate the 32 bit ALU module

---
+++ DigitalCircuitsExperiments/DCE03/ALU32/obj_dir/VALU32__ALL.d
@@ -0,0 +1,15 @@
+VALU32__ALL.o: VALU32__ALL.cpp VALU32.cpp VALU32.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h VALU32__Syms.h \
+ VALU32___024root.h /usr/local/share/verilator/include/verilated_vcd_c.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilated_trace.h \
+ /usr/local/share/verilator/include/verilated_trace_defs.h \
+ VALU32___024root__DepSet_hd6891da2__0.cpp \
+ VALU32___024root__DepSet_h5a5d0907__0.cpp VALU32__Trace__0.cpp \
+ VALU32___024root__Slow.cpp \
+ VALU32___024root__DepSet_h5a5d0907__0__Slow.cpp VALU32__Syms.cpp \
+ VALU32__Trace__0__Slow.cpp
