8c5fd852f911f6cde1350bc7397dafa0aeccbff2
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-01-09 12:33:53 +0800

    Basic modules for a 8-Bit Barrel Shifter
    Implemented 1 2to1 Mux and 24 4to1 Mux.

---
+++ DigitalCircuitsExperiments/DCE06/BarrelShifter8/mux_3b.v
@@ -0,0 +1,15 @@
+module mux_3b(datain, sel, dataout);
+    input [3:0] datain;
+    input [1:0] sel;
+    output reg dataout;
+
+    always @(sel) 
+    begin
+        case (sel)
+        0 : dataout = datain [0];
+        1 : dataout = datain [1];
+        2 : dataout = datain [2];
+        3 : dataout = datain [3];
+        endcase
+    end
+endmodule
---
+++ DigitalCircuitsExperiments/DCE06/BarrelShifter8/mux_c.v
@@ -0,0 +1,13 @@
+module mux_c(datain, sel, dataout);
+    input [1:0] datain;
+    input sel;
+    output reg dataout;
+
+    always @(sel) 
+    begin
+        case (sel)
+        0 : dataout = datain [0];
+        1 : dataout = datain [1];
+        endcase
+    end
+endmodule
