ada41098cd6c7f4c85f6ef3e0ef59ff45d6a52d5
Jasonyanyusong <jasonyanyusong@outlook.com>
2023-01-31 17:14:08 +0800

    removed annotations + remove other nemu dir

--- pa1/nemu/src/monitor/sdb/sdb.c
+++ pa1/nemu/src/monitor/sdb/sdb.c
@@ -57,10 +57,7 @@ static int cmd_si(char *args){
   printf("++++ cmd_si command ++++\n\n");
   printf("==== 让程序单步执行N条指令后暂停执行, 当N没有给出时, 缺省为1 ==== \n");
   printf("==== 程序得到如下子指令: \"%s\" ====\n", args);
-  //char *null_string = "(null)";
-  //printf("#### null_string是: \"%s\" ####\n", null_string);
   if (args == NULL){
-  //if (args.compare(null_string) == 0){
     printf("^^^^ 没有收到子命令，默认执行 1 次 ^^^^\n");
     cpu_exec(1);
   }
@@ -108,54 +105,18 @@ static int cmd_x(char *args){
   int print_length;
   int start_memory_address;
   char *last_part_of_args;
-  //printf("---- 检查点1: 参数args现在是: \"%s\" ----\n", args);
-  //char *string_token_first = strtok(args, " ");
   char *string_token_first = strtok_r(args, " ", &last_part_of_args);
   printf("==== 读取出的string_token_first为: \"%s\" ====\n", string_token_first);
   print_length = atoi(string_token_first);
   printf("==== 转换后的print_length为: \"%d\" ====\n", print_length);
   printf("==== 读取出的last_part_of_args为: \"%s\" ====\n", last_part_of_args);
-  //printf("---- 检查点2: 参数args现在是: \"%s\" ----\n", args);
-  //char *string_token_second = strtok(args, " ");
-  //int decimal_result = 0;
-  //for (int i = (sizeof(last_part_of_args)/sizeof(char)) - 1; i >= 2; i = i - 1)
-  //for (int i = (strlen(last_part_of_args)) - 1; i >= 2; i = i - 1)
-  //{
-  //  decimal_result = decimal_result + (int)pow(16, i - 1) * last_part_of_args[i - 2];
-  //  printf("---- 读出字符数据 %s ----\n", last_part_of_args[i - 2]);
-  //  printf("---- 循环第 %d 次，读出数组数字 %d decimal_result为: %d ----\n", i, last_part_of_args[i - 2], decimal_result);
-  //}
-  //printf("---- 循环完成后，decimal_result为: %d ----\n", decimal_result);
-  //start_memory_address = decimal_result;
   sscanf(last_part_of_args, "%x", &start_memory_address);
   printf("---- 打印内存前，start_memory_address (10进制)为: %d ----\n", start_memory_address);
-  //printf("==== 读取出的string_token_second为: \"%s\" ====\n", string_token_second);
-  //int start_memory_address_atoi;
-  //start_memory_address_atoi = atoi(last_part_of_args);
-  //printf("==== 操作后的start_memory_address_atoi为: \"%d\" ====\n", start_memory_address_atoi);
-  //int start_memory_address_atol;
-  //start_memory_address_atol = atol(last_part_of_args);
-  //printf("==== 操作后的start_memory_address_atol为: \"%d\" ====\n", start_memory_address_atol);
-  //int start_memory_address_atoll;
-  //start_memory_address_atoll = atol(last_part_of_args);
-  //printf("==== 操作后的start_memory_address_atoll为: \"%d\" ====\n", start_memory_address_atoll);
-
-  //char *string_token_second = strtok(args, " ");
-  //printf("==== 读取出的string_token_second为: \"%s\" ====\n", string_token_second);
-  //start_memory_address = atoi(string_token_second);
-  //printf("==== 转换后的start_memory_address为: \"%d\" ====\n", start_memory_address);
-
   for(int i = 0; i < print_length; i = i + 1)
   {
     printf("Address: %x , Data: %lx\n", start_memory_address, paddr_read(start_memory_address, 4));
   }
-
-  //char *string_tokens[] = strtok(args, " ");
-  //printf("==== 读取出的string_tokens[0]为: \"%s\" ====\n", string_tokens[0]);
-  //print_length = atoi(string_tokens[0]);
-  //printf("==== 转换后的print_length为: \"%d\" ====\n", print_length);
-  //printf("==== 读取出的string_tokens[1]为: \"%s\" ====\n", string_tokens[1]);
-  //print_length = atoi(string_tokens[1]);
+  printf("==== 执行完毕,程序退出 ====\n\n");
   return 0;
 }
 
@@ -190,18 +151,15 @@ static struct {
   const char *description;
   int (*handler) (char *);
 } cmd_table [] = {
-  { "help", "打印命令的帮助信息 Display information about all supported commands", cmd_help }, /* OK */
-  { "c", "继续运行被暂停的程序 Continue the execution of the program", cmd_c }, /* OK */
-  { "q", "退出NEMU Exit NEMU", cmd_q }, /* OK */
+  { "help", "打印命令的帮助信息 Display information about all supported commands", cmd_help },
+  { "c", "继续运行被暂停的程序 Continue the execution of the program", cmd_c },
+  { "q", "退出NEMU Exit NEMU", cmd_q },
   { "si", "让程序单步执行N条指令后暂停执行, 当N没有给出时, 缺省为1 Run the program for N steps and then suspend, if N is not given, defalt is 1", cmd_si},
   { "info", "info r: 打印寄存器状态 info w: 打印监视点信息 info r: print the state of register, info w: point the information of monitor point", cmd_info},
   { "x", "求出表达式EXPR的值, 将结果作为起始内存地址, 以十六进制形式输出连续的N个4字节solve the value of EXPR, set the result of the start of memory address, using hexadecimal as output, print N continue 4 Byte", cmd_x},
   { "p", "求出表达式EXPR的值 slove the expression EXPR", cmd_p},
   { "w", "当表达式EXPR的值发生变化时, 暂停程序执行 when the value of EXPR changes, suspend the program", cmd_w},
   { "d", "删除序号为N的监视点 delete the monitor point with number N", cmd_d}
-
-  /* TODO: Add more commands */
-
 };
 
 #define NR_CMD ARRLEN(cmd_table)
