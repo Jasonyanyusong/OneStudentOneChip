c3cd76203d52300ff416892ff7a4fb99544063a6
Jasonyanyusong <jasonyanyusong@outlook.com>
2023-03-23 19:30:13 +0800

    optimized rv32i_AND

--- pa2/nemu_dev/src/isa/riscv64/inst.c
+++ pa2/nemu_dev/src/isa/riscv64/inst.c
@@ -79,7 +79,7 @@ void rv32i_XOR(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word
 void rv32i_SRL(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_SRA(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_OR(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
-void rv32i_AND(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm); // Completed
+void rv32i_AND(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm); // Completed, Optimized
 void rv32i_FENCE(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_FENCE_TSO(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_PAUSE(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
@@ -2205,6 +2205,7 @@ void rv32i_AND(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word
   }
   if(riscv64_instC_Print_Debug)
   {
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] Before Execute:\n");
     // Decimal debug information
     printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_rd (Dec) = %d\n", get_rd);
     printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_src1 (Dec) = %ld\n", get_src1);
@@ -2223,6 +2224,26 @@ void rv32i_AND(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word
     printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_s -> dnpc (Dynamic Next Program Counter) (Hex) = 0x%lx\n", get_s -> dnpc);
   }
   R(get_rd) = get_src1 & get_src2;
+  if(riscv64_instC_Print_Debug)
+  {
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] After Execute:\n");
+    // Decimal debug information
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_rd (Dec) = %d\n", get_rd);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_src1 (Dec) = %ld\n", get_src1);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_src2 (Dec) = %ld\n", get_src2);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_imm (Dec) = %ld\n", get_imm);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_s -> pc (Program Counter) (Dec) = %ld\n", get_s -> pc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_s -> snpc (Static Next Program Counter) (Dec) = %ld\n", get_s -> snpc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_s -> dnpc (Dynamic Next Program Counter) (Dec) = %ld\n", get_s -> dnpc);
+    // Hexadecimal debug information
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_rd (Hex) = 0x%x\n", get_rd);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_src1 (Hex) = 0x%lx\n", get_src1);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_src2 (Hex) = 0x%lx\n", get_src2);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_imm (Hex) = 0x%lx\n", get_imm);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_s -> pc (Program Counter) (Hex) = 0x%lx\n", get_s -> pc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_s -> snpc (Static Next Program Counter) (Hex) = 0x%lx\n", get_s -> snpc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AND(int get_rd, Decode* get_s)] get_s -> dnpc (Dynamic Next Program Counter) (Hex) = 0x%lx\n", get_s -> dnpc);
+  }
   if(riscv64_instC_Print_ChecKPoinT)
   {
     printf("[NEMU_RISCV64_instC CHECKPOINT: void rv32i_AND(int get_rd, Decode* get_s)] CKPT #02: End AND Process Function\n");
