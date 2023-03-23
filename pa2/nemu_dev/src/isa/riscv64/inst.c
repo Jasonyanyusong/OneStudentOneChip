abc13b635a84867650546d7bea7fea56729e7009
Jasonyanyusong <jasonyanyusong@outlook.com>
2023-03-23 17:41:24 +0800

    optimized rv32i_AUIPC

--- pa2/nemu_dev/src/isa/riscv64/inst.c
+++ pa2/nemu_dev/src/isa/riscv64/inst.c
@@ -391,6 +391,7 @@ void rv32i_AUIPC(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, wo
   }
   if(riscv64_instC_Print_Debug)
   {
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] Before Execute:\n");
     // Decimal debug information
     printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_rd (Dec) = %d\n", get_rd);
     printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_src1 (Dec) = %ld\n", get_src1);
@@ -412,6 +413,26 @@ void rv32i_AUIPC(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, wo
   if(riscv64_instC_Print_ChecKPoinT)
   {
     printf("[NEMU_RISCV64_instC CHECKPOINT: void rv32i_AUIPC(int get_rd, Decode* get_s)] CKPT #02: End AUIPC Process Function\n");
+  }
+  if(riscv64_instC_Print_Debug)
+  {
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] After Execute:\n");
+    // Decimal debug information
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_rd (Dec) = %d\n", get_rd);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_src1 (Dec) = %ld\n", get_src1);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_src2 (Dec) = %ld\n", get_src2);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_imm (Dec) = %ld\n", get_imm);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_s -> pc (Program Counter) (Dec) = %ld\n", get_s -> pc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_s -> snpc (Static Next Program Counter) (Dec) = %ld\n", get_s -> snpc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_s -> dnpc (Dynamic Next Program Counter) (Dec) = %ld\n", get_s -> dnpc);
+    // Hexadecimal debug information
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_rd (Hex) = 0x%x\n", get_rd);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_src1 (Hex) = 0x%lx\n", get_src1);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_src2 (Hex) = 0x%lx\n", get_src2);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_imm (Hex) = 0x%lx\n", get_imm);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_s -> pc (Program Counter) (Hex) = 0x%lx\n", get_s -> pc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_s -> snpc (Static Next Program Counter) (Hex) = 0x%lx\n", get_s -> snpc);
+    printf("[NEMU_RISCV64_instC DEBUG: void rv32i_AUIPC(int get_rd, Decode* get_s)] get_s -> dnpc (Dynamic Next Program Counter) (Hex) = 0x%lx\n", get_s -> dnpc);
   }
 }
 
