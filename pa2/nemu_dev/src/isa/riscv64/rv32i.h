3bf38ff11babe9c460163bbbe252d7cdbba45793
Jasonyanyusong <jasonyanyusong@outlook.com>
2023-03-24 09:35:30 +0800

    move jalr from inst.c to jalr.h

--- pa2/nemu_dev/src/isa/riscv64/rv32i.h
+++ pa2/nemu_dev/src/isa/riscv64/rv32i.h
@@ -1,9 +1,9 @@
 #include "lui.h"
 #include "auipc.h"
 #include "jal.h"
+#include "jalr.h"
 
 // RV32I Instructions
-void rv32i_JALR(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm); // Completed, Optimized
 void rv32i_BEQ(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm); // Completed, Optimized
 void rv32i_BNE(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm); // Completed, Optimized
 void rv32i_BLT(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm); // Completed, Optimized
