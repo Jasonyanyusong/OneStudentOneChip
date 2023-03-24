49b980ac57a994a96dc2b8f0994bda972282af0c
Jasonyanyusong <jasonyanyusong@outlook.com>
2023-03-24 09:44:43 +0800

    move andi form inst.c to andi.h

--- pa2/nemu_dev/src/isa/riscv64/rv32i.h
+++ pa2/nemu_dev/src/isa/riscv64/rv32i.h
@@ -6,6 +6,7 @@
 #include "bne.h"
 #include "blt.h"
 #include "addi.h"
+#include "andi.h"
 
 // RV32I Instructions
 void rv32i_BGE(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
@@ -23,7 +24,6 @@ void rv32i_SLTI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, wor
 void rv32i_SLTIU(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_XORI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_ORI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
-void rv32i_ANDI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm); // Completed, Optimized
 void rv32i_SLLI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_SRLI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_SRAI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
