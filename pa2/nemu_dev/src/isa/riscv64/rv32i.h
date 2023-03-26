8c709bf7b208d96ed1932753394b221cb0ff339d
Jasonyanyusong <jasonyanyusong@outlook.com>
2023-03-26 14:15:24 +0800

    Finished XORI

--- pa2/nemu_dev/src/isa/riscv64/rv32i.h
+++ pa2/nemu_dev/src/isa/riscv64/rv32i.h
@@ -14,6 +14,7 @@
 #include "bge.h"
 #include "bltu.h"
 #include "bgeu.h"
+#include "xori.h"
 
 // RV32I Instructions
 void rv32i_LB(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
@@ -26,7 +27,6 @@ void rv32i_SH(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_
 void rv32i_SW(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_SLTI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_SLTIU(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
-void rv32i_XORI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_ORI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_SLLI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
 void rv32i_SRLI(Decode* get_s, int get_rd, word_t get_src1, word_t get_src2, word_t get_imm);
