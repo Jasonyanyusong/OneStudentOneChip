930998b8a5f24e981b55065feb10404d211e3369
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-03-30 11:56:22 +0800

    reconstruct NEMU, fix lui and auipc

--- pa2/nemu_dev/src/isa/riscv64/inst.c
+++ pa2/nemu_dev/src/isa/riscv64/inst.c
@@ -13,7 +13,37 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include "RISCV.h"
+#include <stdio.h>
+#include "local-include/reg.h"
+#include <cpu/cpu.h>
+#include <cpu/ifetch.h>
+#include <cpu/decode.h>
+#include "math.h"
+// #include <stdlib.h>
+
+#define R(i) gpr(i)
+#define Mr vaddr_read
+#define Mw vaddr_write
+
+u_int32_t instruction_value = 0;
+
+enum {
+  TYPE_R, TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J, TYPE_R4, 
+  TYPE_N, // none
+};
+
+bool riscv64_instC_Print_Debug = true;
+bool riscv64_instC_Print_ChecKPoinT = true;
+bool riscv64_instC_Print_Instruction = true;
+
+#define src1R() do { *src1 = R(rs1); } while (0) // Skeleton Code
+#define src2R() do { *src2 = R(rs2); } while (0) // Skeleton Code
+#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0) // Skeleton Code
+#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0) // Skeleton Code
+#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0) // Skeleton Code
+// My codes (Have confusions about SEXT and BITS)
+#define immB() do { *imm = BITS(i, 31, 31) << 12 | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1;} while(0)
+#define immJ() do { *imm = BITS(i, 31, 31) << 20 | BITS(i, 19, 12) << 12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1;} while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -35,8 +65,10 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
 
 static int decode_exec(Decode *s) {
   int rd = 0;
-  word_t src1 = 0, src2 = 0, imm = 0;
+  word_t src1 = 0, src2 = 0, src3 = 0, imm = 0;
   s->dnpc = s->snpc;
+
+  printf("Before Execute: rd=0x%x, R(rd)=0x%lx, src1=0x%lx, src2=0x%lx, src3=0x%lx, imm=0x%lx, pc=0x%lx, dnpc=0x%lx, snpc=0x%lx\n", rd, R(rd), src1, src2, src3, imm, s -> pc, s -> dnpc, s -> snpc);
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
@@ -46,8 +78,8 @@ static int decode_exec(Decode *s) {
 
   INSTPAT_START();
   // RV64I Instructions
-  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, rv32i_LUI(s, rd, src1, src2, imm));    // Good
-  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, rv32i_AUIPC(s, rd, src1, src2, imm));  // Good
+  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm << 12);
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, rv32i_ADDI(s, rd, src1, src2, imm));   // Good
   INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, rv32i_ADD(s, rd, src1, src2, imm));    // Good
   INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and    , R, rv32i_AND(s, rd, src1, src2, imm));    // Good
@@ -75,6 +107,8 @@ static int decode_exec(Decode *s) {
 
   // RV64F, RV64D, RV64Q, RV64Zfh Instructions
 
+  // RV64 Zicsr Zifencei Instructions
+
   // RV64A Instructions
 
   // RV64C Instructions
@@ -86,19 +120,21 @@ static int decode_exec(Decode *s) {
   // RV64H Instructions
 
   // Changed to I, original N (ECALL and EBREAK)
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, rv32i_ECALL(s, rd, src1, src2, imm)); // R(10) is $a0
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , I, rv32i_EBREAK(s, rd, src1, src2, imm)); // R(10) is $a0
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , I, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
 
+  printf("After Execute: rd=0x%x, R(rd)=0x%lx, src1=0x%lx, src2=0x%lx, src3=0x%lx, imm=0x%lx, pc=0x%lx, dnpc=0x%lx, snpc=0x%lx\n", rd, R(rd), src1, src2, src3, imm, s -> pc, s -> dnpc, s -> snpc);
+
   return 0;
 }
 
 int isa_exec_once(Decode *s) {
-  instruction_value = 0;
+  //instruction_value = 0;
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
-  instruction_value = s->isa.inst.val;
+  //instruction_value = s->isa.inst.val;
   return decode_exec(s);
 }
