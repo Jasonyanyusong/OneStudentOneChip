5dd3bc36a4ed9c9f0429f6c528b4786db07e1107
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-03-31 12:11:52 +0800

    edit immB

--- pa2/nemu_dev/src/isa/riscv64/inst.c
+++ pa2/nemu_dev/src/isa/riscv64/inst.c
@@ -42,7 +42,7 @@ bool riscv64_instC_Print_Instruction = true;
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0) // Skeleton Code
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0) // Skeleton Code
 // My codes (Have confusions about SEXT and BITS)
-#define immB() do { *imm = BITS(i, 31, 31) << 12 | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1;} while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1;} while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 19, 12) << 12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1;} while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
