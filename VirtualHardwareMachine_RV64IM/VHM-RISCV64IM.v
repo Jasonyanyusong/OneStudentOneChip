f07dee8562589d0fcce51d204d359723544008ac
Jasonyanyusong <jasonyanyusong@outlook.com>
2023-04-11 18:13:31 +0800

    rename sim_reg to sim_integer_register

--- VirtualHardwareMachine_RV64IM/VHM-RISCV64IM.v
+++ VirtualHardwareMachine_RV64IM/VHM-RISCV64IM.v
@@ -1,5 +1,6 @@
 module RV64IM_VHM(riscv_32bits_instruction, clk);
     input clk; // This is the clock of the VHM.
+
     input [31:0] riscv_32bits_instruction; // This is the only input of RV64IM_VHM, this is the port that the processor receive instruction from simulation and simulator.
 
     reg [63:0] vhm_pc, vhm_dnpc, vhm_snpc; // They are VHM's Program Counter, Dynamic Next Program Counter, Static Next Program Counter.
@@ -9,7 +10,7 @@ module RV64IM_VHM(riscv_32bits_instruction, clk);
     reg [63:0] intreg_x16, intreg_x17, intreg_x18, intreg_x19, intreg_x20, intreg_x21, intreg_x22, intreg_x23;    // Register x16 to x23, XLEN = 64.
     reg [64:0] intreg_x24, intreg_x25, intreg_x26, intreg_x27, intreg_x28, intreg_x29, intreg_x30, intreg_x31;    // Register x24 to x31, XLEN = 64.
 
-    reg [63:0] sim_reg[0:31]; // Registers with XLEN = 64, After executiion, copy sim_reg to intreg.
+    reg [63:0] sim_integer_register[0:31]; // Registers with XLEN = 64, After executiion, copy sim_integer_register to intreg.
 
     reg [7:0] sim_memory[0:268435455]; // Memory size is 0x10000000.
 
@@ -20,10 +21,14 @@ module RV64IM_VHM(riscv_32bits_instruction, clk);
     // For differnt type of instruction, we will create another case model for dealing with the specific function.
     begin
         opcode = riscv_32bits_instruction[6:0];
+
         case (opcode)
             // TODO: the opcode is initialized, for some opcode, we may need another case structure.
         endcase
-        sim_reg[0] = 64'b0000000000000000000000000000000000000000000000000000000000000000; // Register x0($0) is always 0.
+
+        sim_integer_register[0] = 64'b0000000000000000000000000000000000000000000000000000000000000000; // Register x0($0) is always 0.
+        
+        intreg_x00 = sim_integer_register[0];
     end
 
 endmodule
