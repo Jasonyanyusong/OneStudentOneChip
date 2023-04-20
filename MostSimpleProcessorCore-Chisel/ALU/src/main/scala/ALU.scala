f3ff92bb5de675897cb982c6ee7725b3170c65b1
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-04-20 08:15:11 +0800

    A new version of ALU, have all functions but can not pass sbt compile

--- MostSimpleProcessorCore-Chisel/ALU/src/main/scala/ALU.scala
+++ MostSimpleProcessorCore-Chisel/ALU/src/main/scala/ALU.scala
@@ -7,25 +7,45 @@ class ALU extends Module{
         val DataInB = Input(UInt(64.W))
         val OperateCode = Input(UInt(4.W))
         val ComputeResult = Output(UInt(64.W))
-        val CompareResult = Output(Bool(1.W))
-        val ValidOutput = Output(Bool(1.W))
+        val CompareResult = Output(Bool())
+        val ValidOutput = Output(Bool())
+        val IsCompare = Output(Bool())
+        val IsCompute = Output(Bool())
     })
+
     io.ValidOutput := true.B
+
     io.ComputeResult := Mux(
-        OperateCode(3),
-        Mux(OperateCode(2),
-            Mux(OperateCode(1),
-                Mux(OperateCode(0), io.ValidOutput := false.B , io.ValidOutput := false.B),
-                Mux(OperateCode(0), io.ValidOutput := false.B, io.ComputeResult := io.DataInA.S >> io.DataInB)),
-            Mux(OperateCode(1),
-                Mux(OperateCode(0), io.ComputeResult := io.DataInA.S << io.DataInB, io.ComputeResult := io.DataInA.U >> io.DataInB),
-                Mux(OperateCode(0), io.ValidOutput := false.B, io.ValidOutput := false.B))),
-        Mux(OperateCode(2),
-            Mux(OperateCode(1),
-                Mux(OperateCode(0), io.CompareResult := io.DataInA===io.DataInB, io.CompareResult := io.DataInA < io.DataInB),
-                Mux(OperateCode(0), io.ComputeResult := io.DataInA ^ io.DataInB, io.ComputeResult := io.DataInA | io.DataInB)),
-            Mux(OperateCode(1),
-                Mux(OperateCode(0), io.CompareResult := io.DataInA & io.DataInB, io.ComputeResult := ~ io.DataInA),
-                Mux(OperateCode(0), io.ComputeResult := io.DataInA - io.DataInB, io.ComputeResult := io.DataInA + io.DataInB))))
-    io.CompareResult := Mux()
+        io.OperateCode(3),
+        Mux(io.OperateCode(2),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.ValidOutput := false.B , io.ValidOutput := false.B),
+                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.DataInA.asSInt >> io.DataInB)),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.DataInA.asSInt << io.DataInB, io.DataInA.asUInt >> io.DataInB),
+                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.ValidOutput := false.B))),
+        Mux(io.OperateCode(2),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.IsCompute := false.B, io.IsCompute := false.B),
+                Mux(io.OperateCode(0), io.DataInA ^ io.DataInB, io.DataInA | io.DataInB)),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.DataInA & io.DataInB, ~ io.DataInA),
+                Mux(io.OperateCode(0), io.DataInA - io.DataInB, io.DataInA + io.DataInB))))
+
+    io.CompareResult := Mux(
+        io.OperateCode(3),
+        Mux(io.OperateCode(2),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.ValidOutput := false.B , io.ValidOutput := false.B),
+                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.IsCompare := false.B)),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B),
+                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.ValidOutput := false.B))),
+        Mux(io.OperateCode(2),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.DataInA===io.DataInB, io.DataInA < io.DataInB),
+                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B)),
+            Mux(io.OperateCode(1),
+                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B),
+                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B))))
 }
