9c5ca2ad6c00f6d483a255a02c8feb16b3fa9e59
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-04-20 08:37:55 +0800

    ALU.scala OK, test not OK

--- MostSimpleProcessorCore-Chisel/ALU/src/main/scala/ALU.scala
+++ MostSimpleProcessorCore-Chisel/ALU/src/main/scala/ALU.scala
@@ -8,25 +8,20 @@ class ALU extends Module{
         val OperateCode = Input(UInt(4.W))
         val ComputeResult = Output(UInt(64.W))
         val CompareResult = Output(Bool())
-        val ValidOutput = Output(Bool())
-        val IsCompare = Output(Bool())
-        val IsCompute = Output(Bool())
     })
-
-    io.ValidOutput := true.B
 
     io.ComputeResult := Mux(
         io.OperateCode(3),
         Mux(io.OperateCode(2),
             Mux(io.OperateCode(1),
-                Mux(io.OperateCode(0), io.ValidOutput := false.B , io.ValidOutput := false.B),
-                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.DataInA.asSInt >> io.DataInB)),
+                Mux(io.OperateCode(0), 0.U(64.W), 0.U(64.W)),
+                Mux(io.OperateCode(0), 0.U(64.W), io.DataInA.asSInt >> io.DataInB)),
             Mux(io.OperateCode(1),
                 Mux(io.OperateCode(0), io.DataInA.asSInt << io.DataInB, io.DataInA.asUInt >> io.DataInB),
-                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.ValidOutput := false.B))),
+                Mux(io.OperateCode(0), 0.U(64.W), 0.U(64.W)))),
         Mux(io.OperateCode(2),
             Mux(io.OperateCode(1),
-                Mux(io.OperateCode(0), io.IsCompute := false.B, io.IsCompute := false.B),
+                Mux(io.OperateCode(0), 0.U(64.W), 0.U(64.W)),
                 Mux(io.OperateCode(0), io.DataInA ^ io.DataInB, io.DataInA | io.DataInB)),
             Mux(io.OperateCode(1),
                 Mux(io.OperateCode(0), io.DataInA & io.DataInB, ~ io.DataInA),
@@ -36,16 +31,16 @@ class ALU extends Module{
         io.OperateCode(3),
         Mux(io.OperateCode(2),
             Mux(io.OperateCode(1),
-                Mux(io.OperateCode(0), io.ValidOutput := false.B , io.ValidOutput := false.B),
-                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.IsCompare := false.B)),
+                Mux(io.OperateCode(0), 0.U.asBool, 0.U.asBool),
+                Mux(io.OperateCode(0), 0.U.asBool, 0.U.asBool)),
             Mux(io.OperateCode(1),
-                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B),
-                Mux(io.OperateCode(0), io.ValidOutput := false.B, io.ValidOutput := false.B))),
+                Mux(io.OperateCode(0), 0.U.asBool, 0.U.asBool),
+                Mux(io.OperateCode(0), 0.U.asBool, 0.U.asBool))),
         Mux(io.OperateCode(2),
             Mux(io.OperateCode(1),
                 Mux(io.OperateCode(0), io.DataInA===io.DataInB, io.DataInA < io.DataInB),
-                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B)),
+                Mux(io.OperateCode(0), 0.U.asBool, 0.U.asBool)),
             Mux(io.OperateCode(1),
-                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B),
-                Mux(io.OperateCode(0), io.IsCompare := false.B, io.IsCompare := false.B))))
+                Mux(io.OperateCode(0), 0.U.asBool, 0.U.asBool),
+                Mux(io.OperateCode(0), 0.U.asBool, 0.U.asBool))))
 }
